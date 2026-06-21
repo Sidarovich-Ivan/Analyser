e6fe3d477285debe781137ae85497551655a2b5a
Sidarovich Ivan <sidarovich.ivan@gmail.com>
2026-06-21 23:01:48 +0300

    add conversion error handling to int

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
 #include <charconv>
+#include <format>
 #include <stdexcept>
 #include <string>
 #include <string_view>
 
 #include <iostream>
 
-inline int ToInt(std::string_view value) { return std::stoi(value.data()); }
+inline int ToInt(std::string_view value) {
+    try {
+        return std::stoi(value.data());
+    } catch (...) {
+        throw std::runtime_error(std::format("Error in converting {} to int", value));
+    }
+}
